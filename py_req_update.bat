ca8a0ea6bc3562b7091b472370eef7344b44e673
fsecada01 <francis.secada@gmail.com>
2022-02-02 13:22:14 -0500

    gitignore updates

--- py_req_update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Time to make the updates
-rem python -m pip install -U pip pip-tools setuptools wheel -I
-for %%i in (core_requirements, dev_requirements, requirements) do (
-    pip-compile --upgrade %%i.in && copy %%i.txt src
-)
-pip-sync core_requirements.txt dev_requirements.txt
-echo Done!
-@echo on
