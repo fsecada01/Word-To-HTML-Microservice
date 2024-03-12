f51fbe2858843d1ab06419f00e56b674f0f56537
Francis Secada <francis.secada@gmail.com>
2024-03-12 12:49:54 -0400

    patching `convert_to_format` arg requirements

--- src/backend/ConversionTool/core.py
+++ src/backend/ConversionTool/core.py
@@ -14,7 +14,7 @@ Right now, only .docx files are working
 
 
 def _file_type_conv(
-    file_stream: bytes, trans_ele=None, type_name: str = "html"
+    file_stream: bytes, trans_ele: str or None = None, type_name: str = "html"
 ):
     stream = io.BytesIO(file_stream)
     return convert_to_format(
@@ -24,7 +24,11 @@ def _file_type_conv(
     )
 
 
-def convert_to_format(content: bytes | str | io.BytesIO, trans_ele, type_name):
+def convert_to_format(
+    content: bytes | str | io.BytesIO,
+    trans_ele: str | None = None,
+    type_name: str = "html",
+):
     transform_document = transforms.paragraph(trans_ele) if trans_ele else None
     func_name = f"convert_to_{type_name}"
     result = getattr(mammoth, func_name)(
